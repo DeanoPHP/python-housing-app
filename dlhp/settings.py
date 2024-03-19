c093b4dcb014fffb385114e92254b1f6578dac23
DeanoLark <deanolark@gmail.com>
2024-03-19 17:37:38 +0000

    django postgres setup, migrations

--- dlhp/settings.py
+++ dlhp/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'dlhp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dlhpdb',
+        'USER': 'postgres',
+        'PASSWORD': 'jr134244a',
+        'HOST': 'localhost'
     }
 }
 
