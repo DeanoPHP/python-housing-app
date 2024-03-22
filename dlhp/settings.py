134ef4702dd53e80391ae8d142b4903926a8c48b
DeanoLark <deanolark@gmail.com>
2024-03-22 19:49:44 +0000

    Admin Logo and css

--- dlhp/settings.py
+++ dlhp/settings.py
@@ -118,8 +118,8 @@ STATICFILES_DIRS = [
 ]
 
 #Media Folder Settings
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
