d582126dcd08519e017e4872230d401cc1bb6792
DeanoLark <deanolark@gmail.com>
2024-03-25 20:36:07 +0000

    register & login templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        print('SUBMITTED SUCCESSFULLY')
+        # Login user
+        print('moo')
+    else: 
+        return render(request, 'accounts/login.html')
     
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # register user
+        print('sup')
+    else: 
+        return render(request, 'accounts/register.html')
     
-
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
     
